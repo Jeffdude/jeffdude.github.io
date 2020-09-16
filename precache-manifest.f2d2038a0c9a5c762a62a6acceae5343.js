0aac2f5cdbf5e956424e195d54d858e29932d74a
Jeffdude <jeffmilling@gmail.com>
2020-09-16 18:31:59 +0000

    Updates

---
+++ precache-manifest.f2d2038a0c9a5c762a62a6acceae5343.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45eb265c63a809acc01a0b1bb7163c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a10b1e3708c2b3ee928b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "141b99eec79353e49cd4",
+    "url": "/static/js/2.f78f5da5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f78f5da5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10b1e3708c2b3ee928b",
+    "url": "/static/js/main.969e5be8.chunk.js"
+  },
+  {
+    "revision": "0274ef89b1fdcf80d52f",
+    "url": "/static/js/runtime-main.c48da94b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
