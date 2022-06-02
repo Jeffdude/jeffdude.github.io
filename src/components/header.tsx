6256f82d73eebde3de61dbda75f671823884e112
Jeffdude <jeffmilling@gmail.com>
2022-06-02 21:13:55 +0000

    more header formatting + gh pages + resume v1

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 
+import { FaLinkedin, FaGithub } from 'react-icons/fa'; 
+import { GrMail } from 'react-icons/gr';
+import { IoDocumentText } from 'react-icons/io5'
+
 import AtomLogo from '../atom.svg';
 
 function Header(){
   return (
     <div className="App-header">
-      <div className="Header-title">
-        <object data={AtomLogo} aria-label="logo"/>
-        <div>
-          <h2>Jeffrey Milling</h2>
-          <p>Full Stack Developer</p>
+      <div className='App-inner-header'>
+        <div className="Header-title">
+          <object data={AtomLogo} aria-label="logo"/>
+          <div>
+            <h2 id="row">
+              <div style={{fontStyle: "normal", paddingRight: "0.1em"}}>
+                👋
+              </div>
+              Hi, I'm Jeff!
+            </h2>
+            <p>Jeffrey Milling · Full Stack Developer</p>
+            <div id="hline" style={{width: "100%"}}/>
+          </div>
         </div>
-      </div>
-      <div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Header-icons">
+          <div id="cornerbordercontent">
+            <a href="https://www.linkedin.com/in/jeffrey-milling"><FaLinkedin title="My LinkedIn"/></a>
+            <a href="https://github.com/Jeffdude"><FaGithub title="My Github"/></a>
+            <a href={process.env.PUBLIC_URL + "/jeffrey-milling-resume.pdf"}><IoDocumentText title="My Resume"/></a>
+            <a href="mailto:jeffmilling@gmail.com"><GrMail title="Email Me"/></a>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,8 +4,21 @@ import Page from '../components/page';
 
 function HomePage(){
   return <Page>
-    Home Page
+    <div id="leftcol">
+      <h4>About Me:</h4>
+      <p style={{margin: "0.5em 0 0 1.5em"}}>
+        I am a highly driven goal-oriented software engineer with a variety of
+        technical experience aquired through self-teaching, industry experience,
+        and formal education.  After over five years experience in dynamic
+        large-scale system architecture and administration from my time at
+        Facebook and ViaSat Inc., I began pursuing my passion for entrepreneurship
+        and freeskating. I am a co-owner and CTO of JMKRIDE USA. After two and a
+        half years of technical and leadership work, I am in a position where I
+        have begun offloading and sidelining my responsibilities at JMKRIDE and am
+        looking to continue my career with an exciting remote/hybrid role in full
+        stack software development.
+      </p>
+    </div>
   </Page>
 }
-
 export default HomePage;
